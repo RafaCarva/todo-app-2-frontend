be39cac027e6200a3616262feef4e479d11a0b02
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2017-07-21 16:42:14 -0300

    Componentes Grid e IconButton (aula 51)

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
+import Grid from '../template/grid'
+import IconButton from '../template/iconButton'
 
 export default props=>(
     <div role='form' className='todoForm'>
-        <div className='col-xs-12 col-sm-9 col-md-10'>
+        {/* antes, a linha era assim:
+            <div className='col-xs-12 col-sm-9 col-md-10'> */}
+        <Grid cols='12 9 10'>
             <input id='description' className='form-control'
                 placeholder='Adicione uma tarefa'></input>
-        </div>
+        </Grid>
 
-        <div className='col-xs-12 col-sm-3 col-md-2'>
-            <button className='btn btn-primary'>
-                <i className='fa fa-plus'></i>
+        <Grid cols='12 3 2'>
+            {/*
+            antes, a linha era assim:
+            <button className='btn btn-primary'> 
+               <i className='fa fa-plus'></i>
             </button>
-        </div>    
+            */}
+            <IconButton style='primary' icon='plus'></IconButton>
+        </Grid>    
     </div>
 )
