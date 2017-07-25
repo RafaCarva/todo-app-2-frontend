b441bdec43e6c3126784662c3395fcc1ba1eeefd
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2017-07-25 11:28:00 -0300

    Adicionar Teclas de Atalhos (aula 60)

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -18,6 +18,7 @@ export default class Todo extends Component{
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
         this.handleSearch=this.handleSearch.bind(this)
+        this.handleClear=this.handleClear.bind(this)
 
         this.handleMarkAsDone=this.handleMarkAsDone.bind(this)
         this.handleMarkAsPending=this.handleMarkAsPending.bind(this)
@@ -65,6 +66,10 @@ export default class Todo extends Component{
             .then(resp=>this.refresh(this.state.description))
     }
 
+    handleClear(){
+        this.refresh()
+    }
+
     render(){
         return(
             <div>
@@ -76,6 +81,7 @@ export default class Todo extends Component{
                           description={this.state.description}
                           handleChange={this.handleChange}
                           handleSearch={this.handleSearch}
+                          handleClear={this.handleClear}
                 />
                 
                 <TodoList list={this.state.list}
--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
 
-export default props=>(
-    <div role='form' className='todoForm'>
+export default props=>{
+
+    const keyHandler=(e)=>{
+        if(e.key==='Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        }else if(e.key === 'Escape'){
+            props.handleClear()
+        }
+    }
+
+    return(
+         <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
 
             {/*essa description vai como props, mas quando chegar lá
@@ -13,6 +23,7 @@ export default props=>(
                    placeholder='Adicione uma tarefa' 
                    value={props.description} 
                    onChange={props.handleChange}
+                   onKeyUp={keyHandler}
                    >
             </input>
         </Grid>
@@ -28,7 +39,14 @@ export default props=>(
                         onClick={props.handleSearch}>
             </IconButton>
 
+            {/*limpar formulário*/}
+            <IconButton style='default'
+                        icon='close'
+                        onClick={props.handleClear}>
+            </IconButton>
+
         </Grid>  
 
     </div>
-)
+    )
+}
