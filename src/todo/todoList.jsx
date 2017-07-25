77a79acc0f07d32b01b5c66ff93a64162c6e9cca
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2017-07-24 22:54:48 -0300

    Consulta e exclusão de TODOs (aula56)

--- src/todo/todoList.jsx
+++ src/todo/todoList.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props =>(
-    <div>
-        <h1>List</h1>
-    </div>        
-)
+
+export default props =>{
+
+    const renderRows=()=>{
+
+        {/**
+         * list vai receber:
+         * props.list caso exista, se props.list for null
+         * então list recebe um array vazio.
+         */}
+        const list = props.list || []
+
+        return list.map(todo=>(
+            //esse _id é gerado pelo mongo.
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    {/**handleRemove() vai ser criado em todo.jsx*/}
+                    <IconButton style='danger'
+                                icon='trash-o'
+                                onClick={()=>props.handleRemove(todo)}>
+                    </IconButton>
+                </td>
+            </tr>    
+
+        ))
+    }
+
+    return(
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+
+    )
+}
